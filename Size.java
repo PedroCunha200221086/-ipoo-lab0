082b3b8e6d00647f96df552281feb5a1ec71b26c
PedroCunha200221086 <74556906+PedroCunha200221086@users.noreply.github.com>
2021-03-04 15:03:43 +0000

    JAVADOC of 'Size' Class

--- Size.java
+++ Size.java
@@ -7,40 +7,57 @@
  * @version V1.0
  */
 public enum Size
+/**
+ * Definition of enum types(there are three types of cups)
+ */
 {
  SMALL(32, 36, "Pequeno",'S'),
  MEDIUM(37, 44, "Médio",'M'),
  LARGE(45, 52, "Grande",'L');
- 
+ /**
+  * Attributes of enum class 'Size'
+  */
  private final String description;
  private final int minValue;
  private final int maxValue;
  private final char code;
-
+/**
+ * Constructor of enum class 'Size' 
+ */
  private Size(int minValue, int maxValue, String description, char code){
  this.description=description;
  this.minValue=minValue;
  this.maxValue=maxValue;
  this.code=code;
  }
- 
+ /**
+  * This method returns the enum's name
+  */
  @Override
  public String toString() {
  return this.description;
  }
- 
+ /**
+  * This method returns the enum's description
+  */
  public String getDescription() {
  return description;
  }
- 
+ /**
+  * This method returns the enum's minimum value
+  */
  public int getMinValue() {
  return minValue;
  }
- 
+ /**
+  * This method returns the enum's maximum value
+  */
  public int getMaxValue() {
  return maxValue;
  }
- 
+ /**
+  * This method returns the enum's code
+  */
  public char getCode() {
  return code;
  }
