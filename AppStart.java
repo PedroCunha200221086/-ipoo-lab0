f8ec6fa88b4dc59ed0c4817333d0858aaa168208
PedroCunha200221086 <74556906+PedroCunha200221086@users.noreply.github.com>
2021-03-04 17:14:24 +0000

    Bug fixes in switch statement.

--- AppStart.java
+++ AppStart.java
@@ -14,7 +14,7 @@ public class AppStart
         
         Scanner sc = new Scanner(System.in);
         
-        System.out.println("Escolha o tipo de copo: 1- Pequeno | 2- Médio | 3- Grande");
+        System.out.println("Escolha o tipo de bebida: 1- Pequena | 2- Média | 3- Grande\n");
         
         while (sc.hasNext()) {
             
@@ -30,17 +30,17 @@ public class AppStart
             
                 Size small = Size.SMALL;
     
-                System.out.println("Tipo de copo = "+small.toString());
+                System.out.println("Tipo de copo = " + small.toString());
     
-                System.out.println("Ordem = "+small.ordinal());
+                System.out.println("Ordem = " + small.ordinal());
     
-                System.out.println("Descrição = "+small.getDescription());
+                System.out.println("Descrição = " + small.getDescription());
     
-                System.out.println("Valor Mínimo = "+small.getMinValue());
+                System.out.println("Valor Mínimo = " + small.getMinValue());
     
-                System.out.println("Valor Máximo = "+small.getMaxValue());
+                System.out.println("Valor Máximo = " + small.getMaxValue());
     
-                System.out.println("Código = "+small.getCode());
+                System.out.println("Código = " + small.getCode());
                 
                 break;
                 
@@ -48,17 +48,17 @@ public class AppStart
 
                 Size medium = Size.MEDIUM;
     
-                System.out.println("Tipo de copo = "+medium.toString());
+                System.out.println("Tipo de copo = " + medium.toString());
     
-                System.out.println("Ordem = "+medium.ordinal());
+                System.out.println("Ordem = " + medium.ordinal());
     
-                System.out.println("Descrição = "+medium.getDescription());
+                System.out.println("Descrição = " + medium.getDescription());
     
-                System.out.println("Valor Mínimo = "+medium.getMinValue());
+                System.out.println("Valor Mínimo = " + medium.getMinValue());
     
-                System.out.println("Valor Máximo = "+medium.getMaxValue());
+                System.out.println("Valor Máximo = "  + medium.getMaxValue());
     
-                System.out.println("Código = "+medium.getCode());
+                System.out.println("Código = " + medium.getCode());
                 
                 break;
                 
@@ -66,27 +66,27 @@ public class AppStart
             
                 Size large = Size.LARGE;
     
-                System.out.println("Tipo de copo = "+large.toString());
+                System.out.println("Tipo de copo = " + large.toString());
     
-                System.out.println("Ordem = "+large.ordinal());
+                System.out.println("Ordem = " + large.ordinal());
     
-                System.out.println("Descrição = "+large.getDescription());
+                System.out.println("Descrição = " + large.getDescription());
     
-                System.out.println("Valor Mínimo = "+large.getMinValue());
+                System.out.println("Valor Mínimo = " + large.getMinValue());
     
-                System.out.println("Valor Máximo = "+large.getMaxValue());
+                System.out.println("Valor Máximo = " + large.getMaxValue());
     
-                System.out.println("Código = "+large.getCode());
+                System.out.println("Código = " + large.getCode());
                 
                 break;
 
             default:
             
-                System.out.println("Opção inválida! Escolha uma bebida de 1 a 3");
+                System.out.println("Opção inválida! Escolha um tipo de bebida de 1 a 3\n");
                 
-                return;
+                break;
             }
-        }
 
+        }
     }
 }
