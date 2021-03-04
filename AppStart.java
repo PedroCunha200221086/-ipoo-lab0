309d10cd26a47952e912be7250473540ee79bbf0
PedroCunha200221086 <74556906+PedroCunha200221086@users.noreply.github.com>
2021-03-04 16:49:29 +0000

    Bug fixes in class 'AppStart'

--- AppStart.java
+++ AppStart.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 /**
  * This class calls the methods of the class 'Size' and gives an output based on Enum type.
@@ -7,22 +8,85 @@
  */
 public class AppStart
 {
-    public static void main(String[] args) {
+    static Scanner scanner = new Scanner(System.in);
+    
+    public static void  main(String args[]){
         
-    Size medium = Size.MEDIUM;
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Escolha o tipo de copo: 1- Pequeno | 2- Médio | 3- Grande");
+        
+        while (sc.hasNext()) {
+            
+            int select = 0;
+            
+            if (sc.hasNextInt()) {
+                    select = sc.nextInt();
+                }
+                
+            switch (select) {
+
+            case 1:
+            
+                Size small = Size.SMALL;
     
-    System.out.println("Tipo de copo = "+medium.toString());
+                System.out.println("Tipo de copo = "+small.toString());
     
-    System.out.println("Ordem = "+medium.ordinal());
+                System.out.println("Ordem = "+small.ordinal());
     
-    System.out.println("Descrição = "+medium.getDescription());
+                System.out.println("Descrição = "+small.getDescription());
     
-    System.out.println("Valor Mínimo = "+medium.getMinValue());
+                System.out.println("Valor Mínimo = "+small.getMinValue());
     
-    System.out.println("Valor Máximo = "+medium.getMaxValue());
+                System.out.println("Valor Máximo = "+small.getMaxValue());
     
-    System.out.println("Código = "+medium.getCode());
+                System.out.println("Código = "+small.getCode());
+                
+                break;
+                
+            case 2:
+
+                Size medium = Size.MEDIUM;
     
-  }
+                System.out.println("Tipo de copo = "+medium.toString());
     
+                System.out.println("Ordem = "+medium.ordinal());
+    
+                System.out.println("Descrição = "+medium.getDescription());
+    
+                System.out.println("Valor Mínimo = "+medium.getMinValue());
+    
+                System.out.println("Valor Máximo = "+medium.getMaxValue());
+    
+                System.out.println("Código = "+medium.getCode());
+                
+                break;
+                
+            case 3:
+            
+                Size large = Size.LARGE;
+    
+                System.out.println("Tipo de copo = "+large.toString());
+    
+                System.out.println("Ordem = "+large.ordinal());
+    
+                System.out.println("Descrição = "+large.getDescription());
+    
+                System.out.println("Valor Mínimo = "+large.getMinValue());
+    
+                System.out.println("Valor Máximo = "+large.getMaxValue());
+    
+                System.out.println("Código = "+large.getCode());
+                
+                break;
+
+            default:
+            
+                System.out.println("Opção inválida! Escolha uma bebida de 1 a 3");
+                
+                return;
+            }
+        }
+
+    }
 }
